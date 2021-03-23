1001a0efb1177c75f51a41dc1de6a616d53b8af9
Md. Alam <shaiankhan77@gmail.com>
2021-03-23 12:43:50 -0400

    completed the parts for lab 07

---
+++ spanish.py
@@ -0,0 +1,10 @@
+sp = input("Give me a word in English... type Cat, Dog or Horse ")
+
+if sp == "Cat" or sp == "cat":
+	print("gato")	
+elif sp == "Dog" or sp == "dog":
+	print("perro")
+elif sp == "Horse" or sp == "horse":
+	print("caballo")
+else:
+	print("no entiendo") #error message
